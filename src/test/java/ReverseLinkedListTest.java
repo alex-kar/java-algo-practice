0ad03fd5b35e2f44946a4a6721a81e2bcfb566fa
Alex Karpov <alexk@sqreamtech.com>
2022-09-12 18:39:47 -0400

    add 'Reverse linked list'

---
+++ src/test/java/ReverseLinkedListTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// https://leetcode.com/problems/reverse-linked-list/
+class ReverseLinkedListTest {
+
+    @Test
+    public void test1() {
+        ListNode head =
+                new ListNode(1,
+                        new ListNode(2,
+                                new ListNode(3,
+                                        new ListNode(4,
+                                                new ListNode(5)))));
+
+        ListNode result = new ReverseLinkedList().reverseList(head);
+
+        assertEquals(5, result.val);
+        assertEquals(4, result.next.val);
+        assertEquals(3, result.next.next.val);
+        assertEquals(2, result.next.next.next.val);
+        assertEquals(1, result.next.next.next.next.val);
+    }
+
+    @Test
+    public void test2() {
+        assertNull(new ReverseLinkedList().reverseList(null));
+
+    }
+
+
+
+}
