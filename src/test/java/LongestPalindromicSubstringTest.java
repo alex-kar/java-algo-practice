bfe912287109af5e769f57e26d7ab7fad9f70faf
Alex Karpov <alexk@sqreamtech.com>
2022-09-16 11:18:09 -0400

    add 'Longest palindromic substring'

---
+++ src/test/java/LongestPalindromicSubstringTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LongestPalindromicSubstringTest {
+    private LongestPalindromicSubstring test = new LongestPalindromicSubstring();
+
+    @Test
+    public void test1() {
+        assertEquals("aba", test.longestPalindrome("babad"));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals("bb", test.longestPalindrome("cbbd"));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals("baab", test.longestPalindrome("babaabd"));
+    }
+}
